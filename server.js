c7b1a36bf732d131b3a5b9ef92604d42d37f0927
unknown <samuel_66490@ETEC.FURLAN>
2024-09-20 10:51:49 -0300

    add new views: register and not found

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-//Vou instalar o express - 
-//GErenciador de pacotes - NPM
 const express = require ('express')
 const server = express()
+const bodyParser = require('body-parser')
 //Importar para ter acesso ao caminho dos arquivos
 const path = require('path')
 //Expor arquivos estáticos..
 server.use(express.static('public'))
 server.use(express.json()) //Possibilidade de usar JSON
-//Eu desenvolvedor, criei este ENDPOINT
-//http://localhost:3000/cadastro
+
+server.use(bodyParser.urlencoded({extended:true}))
+
 server.get('/cadastro', (req, res) => {
    res.sendFile(path.join(__dirname, 'views/index.html'))
 })
+server.post('/cadastro', (req, res)=>{
+    console.log(req.body)
+    const {email, name} = req.body
+    //CAdastro os dados no banco de dados!
 
+    if (email !== 'muca@email.com') {
+         return res.sendFile(path.join(__dirname, 'views/404.html'))
+    }
+    res.sendFile(path.join(__dirname, 'views/home.html'))
+})
 server.get('/pets', (req,res)=> {
     res.send({
         name: "Meu gato",
@@ -20,8 +29,6 @@ server.get('/pets', (req,res)=> {
         peso: "300kg"
     })
 })
-//Expor para o mundo na porta 3000
 server.listen(3000, () => {
     console.log("servidor no ar...")
-
 })
